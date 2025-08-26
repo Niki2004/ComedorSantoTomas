46675d4e48167d15d62154a63b860eb73f7425bc
Cosmo0004 <108553854+Cosmo0004@users.noreply.github.com>
2025-08-25 21:27:33 -0600

    Al usuario le faltan 2 cosas nada más

---
+++ TCU_Comedor/TCU_Comedor/Controllers/AlimentacionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCU_Comedor.Controllers
+{
+    public class AlimentacionController : Controller
+    {
+        // GET: Alimentacion
+        [Authorize(Roles = "Usuario")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Es para el informe personal 
+        [Authorize(Roles = "Usuario")]
+        public ActionResult IndexInforme()
+        {
+            return View();
+        }
+    }
+}
